b5e3c0841f65be355088180a1ae89c0741da88f9
Raphaël Moreau <rphlmr@users.noreply.github.com>
2017-08-29 18:57:38 +0200

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.us-west-2.rds.amazonaws.com
-export DB_PRD_USER=spidertutorial
-export DB_PRD_PASS=spidertutorial
+export DB_PRD_HOST=demo-postgresql.cmifjcfxd9up.eu-west-2.rds.amazonaws.com
+export DB_PRD_USER=demo
+export DB_PRD_PASS=demo1234
 export NODE_HOST=localhost
-export NODE_PORT=8080
+export NODE_PORT=3000
 node /myapp/index.js&
 exit 0
